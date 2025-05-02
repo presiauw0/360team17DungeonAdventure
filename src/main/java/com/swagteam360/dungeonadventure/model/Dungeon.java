47773ec8d492e67bc0528b340cbabf062b972257
presiauw0 <psia97@uw.edu>
2025-05-02 13:03:47 -0700

    Fix - Refine recursive division maze to make it more reliable.

--- src/main/java/com/swagteam360/dungeonadventure/model/Dungeon.java
+++ src/main/java/com/swagteam360/dungeonadventure/model/Dungeon.java
@@ -1,6 +1,4 @@
-package com.swagteam360.dungeonadventure.controller;
-
-import java.util.Arrays;
+package com.swagteam360.dungeonadventure.model;
 
 public final class Dungeon {
 
@@ -27,32 +25,8 @@ public final class Dungeon {
         if (rowSize > 1 && colSize > 1) {
             final int randRow = (int)(Math.random() * rowSize) + theTop; // where to put horizontal wall
             final int randCol = (int)(Math.random() * colSize) + theLeft; // where to put vertical wall
-            int xGap; // x coordinate of random gap to be placed in the horizontal wall
-            int yGap; // y coordinate of random gap to be placed in the vertical wall
 
-            // determine random gaps but make sure they don't overlap with the other walls
-            do {
-                xGap = (int)(Math.random() * colSize) + theLeft;
-                yGap = (int)(Math.random() * rowSize) + theTop;
-            } while(xGap == randCol || yGap == randRow);
-
-            // construct horizontal wall
-            for (int i = theLeft; i <= theRight; i++) {
-                if (i != xGap) {
-                    myRoomList[randRow][i] = 1;
-                }
-            }
-
-            // construct vertical wall
-            for (int j = theTop; j <= theBottom; j++) {
-                if (j != yGap) {
-                    myRoomList[j][randCol] = 1;
-                }
-            }
-
-            // debug
-            System.out.println(this);
-            System.out.println(xGap + ", " + yGap);
+            buildWalls(theTop, theLeft, theBottom, theRight, randRow, randCol); // Build the wall
 
             // Recursive Calls
             generateDungeon(theTop, theLeft, randRow - 1, randCol - 1);
@@ -61,6 +35,37 @@ public final class Dungeon {
             generateDungeon(randRow + 1, randCol + 1, theBottom, theRight);
 
         }
+    }
+
+    private void buildWalls(final int theTop, final int theLeft,
+                            final int theBottom, final int theRight,
+                            final int randRow, final int randCol) {
+        // Randomly determine where to place gaps in walls
+        final int rowGap1 = randomGen(theLeft, randCol);                // Pick a gap between left extreme and the vertical wall
+        final int rowGap2 = randomGen(randCol + 1, theRight);   // Pick a gap between the vertical wall and the right extreme
+        final int colGap1 = randomGen(theTop, randRow);                 // Pick a gap between the top extreme and the horizontal wall
+        final int colGap2 = randomGen(randRow + 1, theBottom);  // Pick a gap between the horizontal wall and the bottom extreme
+
+        // Determine if the walls are on one of the edges using gap calculations
+        // When walls are on an edge, some of the gaps will equal the wall coordinates
+        final boolean colIsOnEdge = (rowGap1 == randCol || rowGap2 == randCol);
+        final boolean rowIsOnEdge = (colGap1 == randRow || colGap2 == randRow);
+
+        for (int i = theLeft; i <= theRight; i++) {
+            if (i != rowGap1 && i != rowGap2) {
+                myRoomList[randRow][i] = 1;
+            }
+        }
+
+        for (int j = theTop; j <= theBottom; j++) {
+            if (j != colGap1 && j != colGap2) {
+                myRoomList[j][randCol] = 1;
+            }
+        }
+    }
+
+    private int randomGen(final int theStart, final int theEnd) {
+        return (int)(Math.random() * (theEnd - theStart)) + theStart;
     }
 
     private boolean validateMaze() {
