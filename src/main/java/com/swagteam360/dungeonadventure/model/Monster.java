32da88e340e8d05e0f5f1116550f3acb1f210cbe
Luke Willis <lwillis@uw.edu>
2025-05-10 21:54:51 -0700

    Add: Heal method in Monster class.

--- src/main/java/com/swagteam360/dungeonadventure/model/Monster.java
+++ src/main/java/com/swagteam360/dungeonadventure/model/Monster.java
@@ -1,5 +1,7 @@
 package com.swagteam360.dungeonadventure.model;
 
+import java.util.Random;
+
 /**
  * Abtract Class "Monster" defines what a "Monster" is, primarily
  * inheriting characteristics from the super class. A unique quality of "Monster"
@@ -33,4 +35,22 @@ public abstract class Monster extends DungeonCharacter {
         super(theName, theAttackDamage, theHP, theAttackSpeed, theDamageRangeMin, theDamageRangeMax, theHitChance);
         myHealChance = theHealChance;
     }
+
+    /**
+     * 'Heal' method represents Monsters' ability to randomly heal in combat.
+     * Utilizes a random object to simulate the "chance" that a monster will heal.
+     */
+    private void heal() {
+        //establish random object
+        Random random = new Random();
+
+        //roll for heal chance
+        int healRoll = random.nextInt(100) + 1; //roll 1-100
+
+        //if healchance is greater than or equal to the random rolled, will heal :)
+        if (myHealChance >= healRoll) {
+            this.setHP(this.getHP() + healRoll); //set new Health Points
+        }
+    }
+
 }
