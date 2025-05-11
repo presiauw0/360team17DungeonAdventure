6d54af4014bdea93fbcbc0c7771e8162f16d52fb
presiauw0 <psia97@uw.edu>
2025-05-10 23:22:41 -0700

    Feature - Add documentation to the RoomFactory class.

--- src/main/java/com/swagteam360/dungeonadventure/model/RoomFactory.java
+++ src/main/java/com/swagteam360/dungeonadventure/model/RoomFactory.java
@@ -1,11 +1,39 @@
 package com.swagteam360.dungeonadventure.model;
 
+/**
+ * A concrete class that creates Room instances.
+ * Objects are constructed with information on
+ * where the entrance and exit is located, and
+ * creates the proper room type accordingly.
+ *
+ * @author Preston Sia (psia97)
+ * @version 1.00, 10 May 2025
+ */
 public class RoomFactory implements CellFactory {
+    /**
+     * The row that the entrance room will reside.
+     */
     private final int myEntranceRow;
+    /**
+     * The column that the entrance room will reside.
+     */
     private final int myEntranceCol;
+    /**
+     * The row that the exit room will reside.
+     */
     private final int myExitRow;
+    /**
+     * The column where the exit room will reside.
+     */
     private final int myExitCol;
 
+    /**
+     * Creates a room factory which helps to generate rooms.
+     * @param theEntranceRow Entrance room row
+     * @param theEntranceCol Entrance room column
+     * @param theExitRow Exit room row
+     * @param theExitCol Exit room column
+     */
     public RoomFactory(final int theEntranceRow, final int theEntranceCol,
                        final int theExitRow, final int theExitCol) {
 
@@ -17,11 +45,9 @@ public class RoomFactory implements CellFactory {
         myExitCol = theExitCol;
     }
 
+    @Override
     public Cell createCell(final int theRow, final int theCol) {
         Cell roomToReturn;
-        /*
-        return new Room(false, false,
-                theRow, theCol);*/
         if (theRow == myEntranceRow && theCol == myEntranceCol) { // create an entrance room
             roomToReturn = new Room(IRoom.PROPERTY_ENTRANCE, theRow, theCol);
         } else if (theRow == myExitRow && theCol == myExitCol) { // create an exit room
@@ -33,18 +59,22 @@ public class RoomFactory implements CellFactory {
         return roomToReturn;
     }
 
+    @Override
     public int getEntranceRow() {
         return myEntranceRow;
     }
 
+    @Override
     public int getEntranceCol() {
         return myEntranceCol;
     }
 
+    @Override
     public int getExitRow() {
         return myExitRow;
     }
 
+    @Override
     public int getExitCol() {
         return myExitCol;
     }
