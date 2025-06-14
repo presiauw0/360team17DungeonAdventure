793a122e4923528eb2f49212a78ec07c4a330f93
jhernandezzz <jonathanhernandeze05@gmail.com>
2025-06-13 21:09:27 -0700

    Made unit tests for Thief. Created a protected method to be tested on. The public method which override specialMove() simply delegates to this protected method, so I do not believe I need to test this. I also refactored the Thief's special move.

--- src/main/java/com/swagteam360/dungeonadventure/model/Thief.java
+++ src/main/java/com/swagteam360/dungeonadventure/model/Thief.java
@@ -3,16 +3,33 @@ package com.swagteam360.dungeonadventure.model;
 import java.util.Random;
 
 /**
- *  The Thief class is a specific type of Hero, one of the 3 player
- * characters. A Thief has the special ability "Poison Knife" which allows
- * the player to poison the enemy for 5-15 seconds, dealing 5 damage/second.
+ *  The Thief class is a specific type of Hero, one of the 3-player
+ * characters. A Thief has the special ability Surprise attack, which
+ * may guarantee two attacks, one normal attack, or no attack (caught).
  *
  * @author Luke Willis, Jonathan Hernandez
  * @version 4 June 2025
  */
-public class Thief extends Hero{
+public class Thief extends Hero {
+
     /**
-     * Parametered constructor, calls upon the super constructor to help establish the object.
+     * Variable for roll chance
+     */
+    private static final int HUNDRED = 100;
+
+    /**
+     * Variable which represents the chance of a thief being caught.
+     */
+    private static final int PERCENT_CAUGHT = 20;
+
+    /**
+     * Variable that represents the chance of a thief performing a normal attack
+     * instead of a "Surprise Attack". Note that this normal attack can fail as well.
+     */
+    private static final int PERCENT_NORMAL= 60;
+
+    /**
+     * Parameter constructor calls upon the super constructor to help establish the object.
      *
      * @param theName           represents the name of the hero.
      * @param theHP             represents the health points of the hero.
@@ -27,41 +44,35 @@ public class Thief extends Hero{
     }
 
     /**
-     * Thief can launch a surprise attack against a monster. A successful surprise attack will deal two attacks worth
-     * of damage to the monster. An unsuccessful surprise attack may only consist of one attack or a missed attack.
+     * Handles the specialMove of the Thief. A surprise attack guarantees two attacks, but
+     * if the roll chance is smaller, the thief may perform a regular attack or miss.
      *
      * @param theMonster The monster in which the special move may be performed on.
+     * @param theRandom The random number generator used to determine the roll chance.
      * @return A string representation of the outcome of this special move.
      */
-    @Override
-    public String specialMove(final Monster theMonster) {
+    protected String specialMove(final Monster theMonster, final Random theRandom) {
 
-        Random rand = new Random();
-        final int roll = rand.nextInt(100);
+        final int roll = theRandom.nextInt(HUNDRED);
         String result;
 
-        if (roll < 20) {
+        if (roll < PERCENT_CAUGHT) {
             result = "You've been caught! Attack missed!";
-        } else if (roll < 60) {
-            int damage = attack(getDamageRangeMin(), getDamageRangeMax(), getMyHitChance());
-            if (damage > 0) {
-                theMonster.takeDamage(damage);
-                result = "You performed a normal attack for " + damage + " damage!";
-            } else {
-                result = "You tried a surprise attack but missed!";
-            }
+        } else if (roll < PERCENT_NORMAL) {
+            int damage = attack(getDamageRangeMin(), getDamageRangeMax(), HUNDRED); // Guaranteed to hit
+
+            theMonster.takeDamage(damage);
+            result = "You performed a normal attack for " + damage + " damage!";
+
         } else {
             int totalDamage = 0;
-            StringBuilder sb = new StringBuilder("Surprise attack successful!");
+            StringBuilder sb = new StringBuilder("Surprise attack successful! ");
             for (int i = 1; i <= 2; i++) {
-                int damage = attack(getDamageRangeMin(), getDamageRangeMax(), getMyHitChance());
-                if (damage > 0) {
-                    theMonster.takeDamage(damage);
-                    totalDamage += damage;
-                    sb.append("Hit ").append(i).append(": ").append(damage).append(" damage!\n");
-                } else {
-                    sb.append("Hit ").append(i).append(": Missed!\n");
-                }
+                int damage = attack(getDamageRangeMin(), getDamageRangeMax(), HUNDRED); // Guaranteed to hit
+                theMonster.takeDamage(damage);
+                totalDamage += damage;
+                sb.append("Hit ").append(i).append(": ").append(damage).append(" damage!\n");
+
             }
 
             sb.append("Total damage: ").append(totalDamage).append("!");
@@ -70,5 +81,17 @@ public class Thief extends Hero{
 
         return result;
 
+    }
+
+    /**
+     * Thief can launch a surprise attack against a monster. A successful surprise attack will deal two attacks worth
+     * of damage to the monster. An unsuccessful surprise attack may only consist of one attack or a missed attack.
+     *
+     * @param theMonster The monster in which the special move may be performed on.
+     * @return A string representation of the outcome of this special move.
+     */
+    @Override
+    public String specialMove(final Monster theMonster) {
+        return specialMove(theMonster, new Random());
     }
 }
